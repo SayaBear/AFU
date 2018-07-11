dd0b118ad757c2ee17fe4c1f1cbd6fdb4f9aedec
SayaBear <39145906+SayaBear@users.noreply.github.com>
2018-07-11 20:57:57 +0800

    Add files via upload

---
+++ ASFForUbuntu.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+echo "本脚本集成了来自STCN的minimax4233、wolaiwozhengfu的脚本内容并加以修改，向上述二位大佬表示感谢！"
+echo "请确保填写的信息准确，填错了脚本不会有任何提示。";
+echo "请在screen下运行ASF，运行方式是chmod +x ArchiSteamFarm，sudo ./ArchiSteamFarm";
+echo "按Enter继续";
+echo "输入你的帐号";
+read Account
+echo "输入你的密码";
+read Code
+sudo apt-get update
+sudo apt-get install curl libunwind8 gettext
+sudo apt install unzip
+sudo apt install screen
+wget https://github.com/JustArchi/ArchiSteamFarm/releases/download/3.2.0.2/ASF-linux-x64.zip
+unzip ASF-linux-x64.zip -d ASF/
+cd ASF/config
+touch bot1.json
+echo -e "{
+  "\"Enabled\"": true,
+  "\"SteamLogin\"": "\"$Account\"",
+  "\"SteamPassword\"": "\"$Code\"",
+  "\"IsBotAccount\"": false,
+}" > /root/ASF/config/bot1.json
+echo -e "{
+  "\"CurrentCulture\"": "\"zh-CN\""
+}" > /root/ASF/config/ASF.json
+cd /root/ASF
+screen -S ASF
