c2195aefaec959294f1b62384586b871547fed0b
SayaBear <39145906+SayaBear@users.noreply.github.com>
2018-08-20 22:25:11 +0800

    Update AFU.bash
    本次更新主要内容：
    1.适配最新的稳定版V3.0.3.3版本（可在新版Steam聊天室内发送指令）
    2.加入组命令功能，目前仅支持单个账号（大号）挂卡及发送组命令，暂不支持加入小号（主要是我还不会）

--- AFU.bash
+++ AFU.bash
@@ -8,19 +8,41 @@ read Account
 echo "输入你的密码";
 read Code
 sudo apt-get update
-sudo apt-get install curl libunwind8 gettext
+sudo apt-get install libicu-dev
 sudo apt install unzip
 sudo apt install screen
-wget https://github.com/JustArchi/ArchiSteamFarm/releases/download/3.2.0.2/ASF-linux-x64.zip
+wget https://github.com/JustArchi/ArchiSteamFarm/releases/download/3.3.0.3/ASF-linux-x64.zip
 unzip ASF-linux-x64.zip -d ASF/
-cd ASF/config
-touch bot.json
-echo -e "{
-  "\"Enabled\"": true,
-  "\"SteamLogin\"": "\"$Account\"",
-  "\"SteamPassword\"": "\"$Code\"",
-  "\"IsBotAccount\"": false,
-}" > /root/ASF/config/bot.json
+echo -e "是否需要开启通过steam组输入指令功能？ \n1.开启 \n2.不开启"
+read -r -p "请输入数字" GP
+case $GP in
+  1)echo "输入你的steam64位id"
+    read ID
+    echo "输入你的组id"
+    read Group
+    cd ASF/config
+    touch bot.json
+    echo -e "{
+    "\"Enabled\"": true,
+    "\"SteamLogin\"": "\"$Account\"",
+    "\"SteamPassword\"": "\"$Code\"",
+    "\"IsBotAccount\"": false,
+    "\"s_SteamMasterClanID\"": "\"$Group\"",
+    "\"SteamUserPermissions\"": {
+      "\"$ID\"": 3
+    }
+  }" > /root/ASF/config/bot.json
+  ;;
+  2)cd ASF/config
+    touch bot.json
+    echo -e "{
+    "\"Enabled\"": true,
+    "\"SteamLogin\"": "\"$Account\"",
+    "\"SteamPassword\"": "\"$Code\"",
+    "\"IsBotAccount\"": false
+  }" > /root/ASF/config/bot.json
+  ;;
+esac
 echo -e "{
   "\"CurrentCulture\"": "\"zh-CN\"",
   "\"UpdateChannel\"": 0,
